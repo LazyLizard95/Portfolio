1f7c603c74b2cf766b14afdc37d113574dda88dc
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-08 14:25:18 -0400

    Major Reorganizing
    -PlaceHolder images added
    -Links to GitHub and LinkedIn added.
    -MailTo feature added.
    -Somewhat bug-fixed the textbox
    -FlexBox Mostly implemented.
    -Removed Primitive Banner
    -ReadMe Updated with sources for images and fonts.

--- main.js
+++ main.js
@@ -1,54 +1,62 @@
-let userInput = document.getElementById("main");
-let display = document.querySelector(".page.is-active");
 window.onload = () => {
 const tab_switchers = document.querySelectorAll('[data-switcher]');
+const userInput = document.getElementById("main");
+
+
+
+userInput.addEventListener('keyup', (event) => {
+    let current_page = document.querySelector('.pages .page.is-active');
+    const userInput = document.getElementById("main");
+    if (event.key == 'Enter'){
+        if(userInput.value == "about"){
+            current_page.classList.remove('is-active');
+            let next_page = document.querySelector(`.pages .page[data-page="3"]`);
+            next_page.classList.add('is-active');
+            current_page = next_page;
+            console.log("I hear you! 1");
+            
+        }
+    }
+
+    if (event.key == 'Enter'){
+        if(userInput.value == "projects"){
+            current_page.classList.remove('is-active');
+            let next_page = document.querySelector(`.pages .page[data-page="2"]`);
+            next_page.classList.add('is-active');
+            current_page = next_page;
+            console.log("I hear you! 2");
+            
+        }
+    }
+    
+    if (event.key == 'Enter'){
+        if(userInput.value == "contact"){
+            current_page.classList.remove('is-active');
+            let next_page = document.querySelector(`.pages .page[data-page="4"]`);
+            next_page.classList.add('is-active');
+            current_page = next_page;
+            console.log("I hear you! 3");
+            
+        
+        }
+    }
+    
+    if (event.key == 'Enter'){
+        if(userInput.value == "help"){
+            alert("Lost are we? Commands I am willing to remind you of include the following: About, Projects, and Contact.")
+            
+        }
+    }
+
+});  
+
 
 
 for (let i = 0; i < tab_switchers.length; i++) {
     const tab_switcher = tab_switchers[i];
     const page_id = tab_switcher.dataset.tab;
 
-    userInput.addEventListener('keyup', (event) => {
-        const current_page = document.querySelector('.pages .page.is-active');
-        if (event.key == 'Enter'){
-            if(userInput.value == "about"){
-                current_page.classList.remove('is-active');
-                const next_page = document.querySelector(`.pages .page[data-page="1"]`);
-                next_page.classList.add('is-active');
-                console.log("I hear you! 1");
-                document.getElementById("main");
-            }
-        }
     
-        if (event.key == 'Enter'){
-            if(userInput.value == "projects"){
-                current_page.classList.remove('is-active');
-                const next_page = document.querySelector(`.pages .page[data-page="2"]`);
-                next_page.classList.add('is-active');
-                console.log("I hear you! 2");
-                document.getElementById("main");
-            }
-        }
-        
-        if (event.key == 'Enter'){
-            if(userInput.value == "contact"){
-                current_page.classList.remove('is-active');
-                const next_page = document.querySelector(`.pages .page[data-page="3"]`);
-                next_page.classList.add('is-active');
-                console.log("I hear you! 3");
-                document.getElementById("main");
-            
-            }
-        }
-        
-        if (event.key == 'Enter'){
-            if(userInput.value == "help"){
-                
-            
-            }
-        }
-    
-    });  
     
      tab_switcher.addEventListener('click', () =>{
         document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
